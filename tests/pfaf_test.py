646b7568be138cced641186cca279ec7eff7d3c6
TonJoe <scgyzzz@gmail.com>
2025-03-06 13:53:29 +0800

    adding pfaffian_test.py, for training pfaffian

---
+++ tests/pfaf_test.py
@@ -0,0 +1,13 @@
+from deephall import Config, train
+from deephall.config import Network, NetworkType, System,PsiformerNetwork
+
+
+
+config = Config(network=Network(type=NetworkType.pfaffian))
+config.system.nspins = (4, 0)
+config.system.flux = 5
+config.system.interaction_strength = 1.0
+config.optim.iterations = 100
+config.batch_size = 100
+
+train(config)
