4d9fb9e441fbfbed1ce43b9b01ff52bafe77420e
TonJoe <scgyzzz@gmail.com>
2025-04-04 10:12:43 +0800

    modified pfaf_test.py

--- tests/pfaf_test.py
+++ tests/pfaf_test.py
@@ -4,12 +4,23 @@ import jax
 # jax.config.update("jax_debug_nans", True)
 
 
+for Ne in [8,]:
+    config = Config(network=Network(type=NetworkType.pfaffian))
+    config.system.nspins = (Ne, 0)
+    config.system.flux = 2*Ne+1
+    config.system.interaction_strength = 1.0
+    config.optim.iterations = 5000
+    config.batch_size = 256
+    config.mcmc.width = 0.3
+    config.log.save_path = "pfaffian"+str(Ne)
+    train(config)
 
-config = Config(network=Network(type=NetworkType.pfaffian))
-config.system.nspins = (6, 0)
-config.system.flux = 13
-config.system.interaction_strength = 1.0
-config.optim.iterations = 50000
-config.batch_size = 256
-config.mcmc.width = 0.3
-train(config)
+    config = Config(network=Network(type=NetworkType.psiformer))
+    config.system.nspins = (Ne, 0)
+    config.system.flux = 2*Ne+1
+    config.system.interaction_strength = 1.0
+    config.optim.iterations = 5000
+    config.batch_size = 256
+    config.mcmc.width = 0.3
+    config.log.save_path = "psiformer"+str(Ne)
+    train(config)
