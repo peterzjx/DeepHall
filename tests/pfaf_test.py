012dd77d81cdf1a4fe0b4ded92514e21c45384bf
TonJoe <scgyzzz@gmail.com>
2025-04-19 11:54:48 +0800

    nothing really changed

--- tests/pfaf_test.py
+++ tests/pfaf_test.py
@@ -5,12 +5,12 @@ import jax
 
 
 for Ne in [4,6,8]:
-    for kappa in [0.3, 0.5, 2, 4]:
+    for kappa in [0.5, 2, 4]:
         config = Config(network=Network(type=NetworkType.pfaffian))
         config.system.interaction_strength = kappa
         config.system.nspins = (Ne, 0)
         config.system.flux = 2*Ne+1
-        config.optim.iterations = 5000
+        config.optim.iterations = 10000
         config.batch_size = 128
         config.mcmc.width = 0.3
         config.log.save_path = "pfaffian"+str(Ne)+"kappa"+str(kappa)
@@ -20,7 +20,7 @@ for Ne in [4,6,8]:
         config.system.interaction_strength = kappa
         config.system.nspins = (Ne, 0)
         config.system.flux = 2*Ne+1
-        config.optim.iterations = 5000
+        config.optim.iterations = 10000
         config.batch_size = 128
         config.mcmc.width = 0.3
         config.log.save_path = "psiformer"+str(Ne)+"kappa"+str(kappa)
