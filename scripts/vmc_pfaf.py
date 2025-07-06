6b5ac8e114471dd04463ee3c105834f50e07501c
TonJoe <scgyzzz@gmail.com>
2025-07-06 16:35:35 +0800

    added vmc_pfaf.py

---
+++ scripts/vmc_pfaf.py
@@ -0,0 +1,38 @@
+from deephall import Config, train
+from deephall.config import Network, NetworkType, FluxType, FermionicType, PartonNetwork
+import jax
+from datetime import datetime
+# jax.config.update("jax_debug_nans", True)
+
+
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+for Ne in [4]:
+    for kappa in [1.0]:
+        config = Config(network=Network(
+            type=NetworkType.parton,
+            parton=PartonNetwork(
+                fermionic_type=FermionicType.pfaffian,
+                flux_type=FluxType.symmetric_mlp_network
+            )
+        ))
+        config.system.interaction_strength = kappa
+        config.system.nspins = (Ne, 0)
+        config.system.flux = 2*Ne+1
+        config.optim.iterations  = 100000
+        config.batch_size = 1024
+        config.mcmc.width = 0.5
+        # config.log.pretrained_path = f"20250418221957_pfaffian_4_kappa_0.5"
+        # config.log.save_path = f"{timestamp}_pfaffian_{Ne}_kappa_{kappa}"
+        config.log.save_path = f"../logs/pfaf_{Ne}_kappa_{kappa}"
+        train(config)
+
+        # config = Config(network=Network(type=NetworkType.psiformer))
+        # config.system.interaction_strength = kappa
+        # config.system.nspins = (Ne, 0)
+        # config.system.flux = 2*Ne+1
+        # config.optim.iterations = 8000
+        # config.batch_size = 128
+        # config.mcmc.width = 0.3
+        # # config.log.save_path = f"{timestamp}_psiformer_{Ne}_kappa_{kappa}"
+        # config.log.save_path = f"psiformer_{Ne}_kappa_{kappa}"
+        # train(config)
