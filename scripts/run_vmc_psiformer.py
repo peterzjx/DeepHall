149e7c31033c6633dc20d17ced0a1e984d79a722
peter <peterzjx@gmail.com>
2025-09-21 22:31:21 -0700

    add todo for multistepstate fix

--- scripts/run_vmc_psiformer.py
+++ scripts/run_vmc_psiformer.py
@@ -20,14 +20,13 @@ for Ne in [4]:
         config.system.nspins = (Ne, 0)
         config.system.flux = twoQ
         config.optim.optimizer = OptimizerName.adam
-        config.optim.adam.gradient_accumulation_steps = 10
+        # config.optim.adam.gradient_accumulation_steps = 10
         config.optim.iterations  = 100000
-        config.batch_size = 1024
+        config.batch_size = 128
         config.mcmc.width = 0.5
-        # config.log.pretrained_path = f"../tasks/psiformer_N{Ne}_kappa{kappa}/ckpt_{config.optim.iterations-1:07d}.npz"
-        config.log.pretrained_path = "../logs/psiformer_laughlin_fit/ckpt_019999.npz"
-        # config.log.save_path = f"{timestamp}_pfaffian_{Ne}_kappa_{kappa}"
-        config.log.save_path = f"../tasks/from_pretrain_psiformer_{Ne}_kappa_{kappa}"
+        # config.log.pretrained_path = "logs/psiformer_laughlin_fit/ckpt_005256.npz"
+        # config.log.pretrained_path = "tasks/from_pretrain_psiformer_4_kappa_1.0/ckpt_000298.npz"
+        config.log.save_path = f"tasks/from_pretrain_psiformer_{Ne}_kappa_{kappa}_3"
         train(config)
 
         # config = Config(network=Network(type=NetworkType.psiformer))
