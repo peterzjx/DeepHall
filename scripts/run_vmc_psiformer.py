343c0170eb2492ffa59cf1f436d1425e632f2c26
TonJoe <scgyzzz@gmail.com>
2025-09-22 16:10:07 +0800

    minor modification, found bug in restoring from multistep adam optimization

--- scripts/run_vmc_psiformer.py
+++ scripts/run_vmc_psiformer.py
@@ -25,18 +25,7 @@ for Ne in [4]:
         config.batch_size = 1024
         config.mcmc.width = 0.5
         # config.log.pretrained_path = f"../tasks/psiformer_N{Ne}_kappa{kappa}/ckpt_{config.optim.iterations-1:07d}.npz"
-        config.log.pretrained_path = "../logs/psiformer_laughlin_fit/ckpt_019999.npz"
+        # config.log.pretrained_path = "../logs/psiformer_laughlin_fit/ckpt_019999.npz"
         # config.log.save_path = f"{timestamp}_pfaffian_{Ne}_kappa_{kappa}"
-        config.log.save_path = f"../tasks/from_pretrain_psiformer_{Ne}_kappa_{kappa}"
+        config.log.save_path = f"../logs/from_pretrain_psiformer_{Ne}_kappa_{kappa}"
         train(config)
-
-        # config = Config(network=Network(type=NetworkType.psiformer))
-        # config.system.interaction_strength = kappa
-        # config.system.nspins = (Ne, 0)
-        # config.system.flux = 2*Ne+1
-        # config.optim.iterations = 8000
-        # config.batch_size = 128
-        # config.mcmc.width = 0.3
-        # # config.log.save_path = f"{timestamp}_psiformer_{Ne}_kappa_{kappa}"
-        # config.log.save_path = f"psiformer_{Ne}_kappa_{kappa}"
-        # train(config)
